1f6b07e9705070584560ed8845a849fe84d36e4d
dawid.franzoso <dawid.franzoso@gmail.com>
2025-11-26 13:33:47 +0100

    Adding ZIH job support

--- start_job.sh
+++ start_job.sh
@@ -1,1 +1,1 @@
-salloc --gres=gpu:1 -c 1 --nodes=1 --mem=20G --partition=capella-interactive echo "Hello"
+salloc --gres=gpu:1 -c 1 --nodes=1 --mem=20G --partition=capella-interactive python main.py
